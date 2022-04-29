cdfef0e246cc6a59c7037e8f6cbfa384ac5d94b0
Roman Bukin <roman@vanbukin.com>
2022-04-29 20:27:45 +0300

    Refactoring headers extensions. Set headers values with indexer.

--- src/IdentityEngine/Extensions/HttpResponseExtensions.cs
+++ src/IdentityEngine/Extensions/HttpResponseExtensions.cs
@@ -10,28 +10,14 @@ public static class HttpResponseExtensions
     public static void SetNoCache(this HttpResponse response)
     {
         ArgumentNullException.ThrowIfNull(response);
-        if (!response.Headers.ContainsKey("Cache-Control"))
-        {
-            response.Headers.Add("Cache-Control", "no-store, no-cache, max-age=0");
-        }
-        else
-        {
-            response.Headers["Cache-Control"] = "no-store, no-cache, max-age=0";
-        }
-
-        if (!response.Headers.ContainsKey("Pragma"))
-        {
-            response.Headers.Add("Pragma", "no-cache");
-        }
+        response.Headers["Cache-Control"] = "no-store, no-cache, max-age=0";
+        response.Headers["Pragma"] = "no-cache";
     }
 
     public static void SetNoReferrer(this HttpResponse response)
     {
         ArgumentNullException.ThrowIfNull(response);
-        if (!response.Headers.ContainsKey("Referrer-Policy"))
-        {
-            response.Headers.Add("Referrer-Policy", "no-referrer");
-        }
+        response.Headers["Referrer-Policy"] = "no-referrer";
     }
 
     public static void AddScriptCspHeaders(this HttpResponse response, ContentSecurityPolicyOptions cspOptions, string hash)
@@ -44,15 +30,10 @@ public static class HttpResponseExtensions
             ContentSecurityPolicyLevel.Two => $"default-src 'none'; script-src '{hash}'",
             _ => throw new ArgumentOutOfRangeException(nameof(cspOptions.Level), "Invalid content security policy level")
         };
-
-        if (!response.Headers.ContainsKey("Content-Security-Policy"))
+        response.Headers["Content-Security-Policy"] = cspHeader;
+        if (cspOptions.AddDeprecatedHeader)
         {
-            response.Headers.Add("Content-Security-Policy", cspHeader);
-        }
-
-        if (cspOptions.AddDeprecatedHeader && !response.Headers.ContainsKey("X-Content-Security-Policy"))
-        {
-            response.Headers.Add("X-Content-Security-Policy", cspHeader);
+            response.Headers["X-Content-Security-Policy"] = cspHeader;
         }
     }
 
